7c12a87d8a9caa981510a63700b95f597a61a5e6
Alejandro Castro de la Guardia <guardiadelcastro@gmail.com>
2021-11-30 10:03:47 +0000

    finished tutorial

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -1,22 +1,22 @@
-import { useLoaderData } from 'remix'
+import { useLoaderData, Link } from 'remix'
+import { Post, getPosts } from '../../post'
 
 export let loader = () => {
-  return [
-    {
-      slug: 'my-first-post',
-      title: 'My First Post',
-    },
-    {
-      slug: '90s-mixtape',
-      title: 'A Mixtape I Made Just For You',
-    },
-  ]
+  return getPosts()
 }
 
 export default function Posts() {
+  let posts = useLoaderData<Post[]>()
   return (
     <div>
       <h1>Posts</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.slug}>
+            <Link to={post.slug}>{post.title}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
